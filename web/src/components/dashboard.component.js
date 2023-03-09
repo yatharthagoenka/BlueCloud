d0a9a171f6a981a4133473dd6aa483b47789cc8c
Yathartha Goenka <75240973+yatharthagoenka@users.noreply.github.com>
2023-03-09 14:58:25 +0530

    init web interface

---
+++ web/src/components/dashboard.component.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Navigate } from "react-router-dom";
+import AuthService from "../services/auth.service";
+
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: null,
+      userReady: false,
+      currentUser: { username: "" }
+    };
+  }
+
+  componentDidMount() {
+    const currentUser = AuthService.getCurrentUser();
+    console.log(currentUser)
+
+    if (!currentUser) this.setState({ redirect: "/home" });
+    this.setState({ currentUser: currentUser, userReady: true })
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Navigate to={this.state.redirect} />
+    }
+
+    const { currentUser } = this.state;
+
+    return (
+      <div className="container">
+        {(this.state.userReady) ?
+        <div>
+        <header className="jumbotron">
+          <h3>
+            <strong>{currentUser.user.username}</strong>
+          </h3>
+        </header>
+        <p>
+          <strong>Token:</strong>{" "}
+          {currentUser.token.substring(0, 20)} ...{" "}
+          {currentUser.token.substr(currentUser.token.length - 20)}
+        </p>
+        <p>
+          <strong>Id:</strong>{" "}
+          {currentUser.user._id}
+        </p>
+        <p>
+          <strong>Email:</strong>{" "}
+          {currentUser.user.email}
+        </p>
+      </div>: null}
+      </div>
+    );
+  }
+}
